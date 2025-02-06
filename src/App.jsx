677371c717cb3c4b3fc4051f93646e8f577e8831
minae-yang <minae.yang@mcbglobal.pro>
2025-02-06 16:26:30 +0900

    자식 라우터 재설정(auth 부분)

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Calendar from "./pages/Calendar";
 import Community from "./pages/Community";
@@ -12,6 +12,8 @@ import { loginSuccess, logout } from "./redux/actions/authActions";
 import { auth } from "./firebase/firebase";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
+import LoginForm from "./components/auth/LoginForm";
+import SignUpForm from "./components/auth/SignUpForm";
 
 const App = () => {
   useEffect(() => {
@@ -34,7 +36,11 @@ const App = () => {
           <Route path="/community" element={<Community />}>
             <Route path=":postId" element={<CommunityDetail />} />
           </Route>
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/auth" element={<Auth />}>
+            <Route index element={<Navigate to="/auth/login" replace />} />
+            <Route path="login" element={<LoginForm />} />
+            <Route path="signup" element={<SignUpForm />} />
+          </Route>
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/news" element={<News />} />
         </Routes>
