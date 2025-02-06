4447d39f355a4edc77ffbf4a6cbe8a5239c74777
minae <ydmaad@gmail.com>
2025-02-06 17:28:44 +0900

    App.jsx: 로그인 상태인지 아닌지 콘솔 추가/ Home.jsx: 로그아웃 버튼 추가

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { signOutUser } from "../firebase/auth";
+import { logout } from "../redux/actions/authActions";
 
 const Home = () => {
-  return <div>Home</div>;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  console.log("현재 인증 상태:", isAuthenticated);
+  console.log("현재 사용자 정보:", user);
+
+  const handleLogout = async () => {
+    try {
+      const { error } = await signOutUser();
+      if (error) {
+        console.log("로그아웃 실패:", error);
+        return;
+      }
+      dispatch(logout());
+      navigate("/auth/login");
+    } catch (error) {
+      console.error("로그아웃 중 에러 발생:", error);
+    }
+  };
+  return (
+    <div>
+      <h1>홈페이지</h1>
+      {isAuthenticated && <button onClick={handleLogout}>로그아웃</button>}
+    </div>
+  );
 };
 
 export default Home;
