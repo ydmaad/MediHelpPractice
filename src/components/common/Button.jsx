8567fdc8d3dc3b9691300272a04e4189e2dd57f3
minae <ydmaad@gmail.com>
2025-02-13 18:39:27 +0900

    fix: 버튼 컴포넌트에 프롭스 추가-size

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 
-const Button = ({ children, onClick, type = "button", disabled = false }) => {
+const Button = ({
+  children,
+  onClick,
+  type = "button",
+  disabled = false,
+  size,
+}) => {
   return (
     <button
       onClick={onClick}
       type={type}
       disabled={disabled}
-      className="rounded-md bg-primary/500 px-6 py-2 text-header-16 text-white hover:bg-primary/600 transition-colors"
+      className={`rounded-md bg-primary/500 px-6 py-2 text-header-16 text-white hover:bg-primary/600 transition-colors ${size}`}
     >
       {children}
     </button>
