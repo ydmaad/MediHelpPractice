e1fea6bf32d57ebeadf916f6e2d2bc7f0d17dda9
minae <ydmaad@gmail.com>
2025-02-13 15:06:01 +0900

    fix: 레아아웃 너비 변경

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -4,10 +4,10 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-bg font-pretendard tracking-tight">
       <Header />
       <main className="flex-1">
-        <div className="w-[78%] max-w[1000px] mx-auto ">{children}</div>
+        <div className="w-[1000px] mx-auto ">{children}</div>
       </main>
       <Footer />
     </div>
