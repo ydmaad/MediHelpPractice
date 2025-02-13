28e4c87e47e801436af77c2a5228fd263ab30e24
minae-yang <minae.yang@mcbglobal.pro>
2025-02-13 15:05:07 +0900

    add: CommunityCard.jsx: 커뮤니티 게시글 카드 생성 / Community.jsx:커뮤니티 페이지 테스트 중

---
+++ src/components/common/CommunityCard.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { RxDividerVertical } from "react-icons/rx";
+
+const CommunityCard = ({
+  category,
+  title,
+  commentCount,
+  content,
+  author,
+  time,
+  likeCount,
+  image,
+  onClick,
+}) => {
+  return (
+    <div
+      className="bg-white hover:cursor-pointer hovor:bg-gray/100 transition-colors mt-5  rounded-2xl"
+      onClick={onClick}
+    >
+      <div className="flex justify-between items-center p-5 h-40 ">
+        <div className="flex flex-col justify-between h-full">
+          <div>
+            <div className="text-gray/400 text-body-14">{category}</div>
+            <div className="flex items-center">
+              <div className="text-gray/1000 text-header-18 pb-1 pt-2">
+                {title}
+              </div>
+              <div className="text-[#F66555] text-header-18 ml-1">
+                ({commentCount})
+              </div>
+            </div>
+            <div className="text-gray/800 text-body-14">{content}</div>
+          </div>
+
+          <div className="flex items-center">
+            <div className="text-gray/600 text-body-14">{author}</div>
+            <RxDividerVertical className="text-gray/600" />
+            <div className="text-gray/600 text-body-14">{time}</div>
+            <RxDividerVertical className="text-gray/600" />
+            <div className="text-gray/600 text-body-14">저장 {likeCount}</div>
+          </div>
+        </div>
+        {image && (
+          <div className="w-[120px] h-[120px] border-2 overflow-hidden rounded-xl">
+            <img
+              src={image}
+              alt="대표이미지"
+              className="w-full h-full object-cover"
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CommunityCard;
--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -1,12 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import CommunityCard from "../components/common/CommunityCard";
+import Button from "../components/common/Button";
+import 약이미지 from "../assets/약이미지.jpg";
 
 const Community = () => {
+  // const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const { isAuthenticated } = useSelector((state) => state.auth);
+  const posts = [
+    {
+      id: 1,
+      title:
+        "정수리 탈모로 이 약 먹어보신 분 계신가요? 저는 이런 저런 약을 먹고 있습니다!!",
+      content: "요즘 정수리 쪽이 점점 비는 것 같아서 복용 중입니다...",
+      author: "용감한사자",
+      time: "2025년 2월 12일",
+      commentCount: 37,
+      likeCount: 14,
+      image: 약이미지,
+      category: "궁금해요",
+    },
+    {
+      id: 2,
+      title: "정수리 탈모로 이 약 먹어보신 분 계신가요?",
+      content:
+        "요즘 정수리 쪽이 점점 비는 것 같아서 복용 중입니다... 어떤약을 먹는게 좋을까요?? 추천 좀 부탁드릴게요!!!",
+      author: "용감한사자",
+      time: "2025년 2월 12일",
+      commentCount: 37,
+      likeCount: 14,
+      category: "궁금해요",
+    },
+    {
+      id: 3,
+      title: "정수리 탈모로 이 약 먹어보신 분 계신가요?",
+      content: "요즘 정수리 쪽이 점점 비는 것 같아서 복용 중입니다...",
+      author: "용감한사자",
+      time: "2025년 2월 12일",
+      commentCount: 37,
+      likeCount: 14,
+      image: "/path/to/image.jpg",
+      category: "궁금해요",
+    },
+  ];
   return (
     <div>
-      <h1>Community</h1>
-      <div className="">
-        <div>여기는 제목</div>
-        <div>여기는 글 내용</div>
+      <div>
+        <h1>Community</h1>
+        <Button>글쓰기</Button>
+      </div>
+
+      <div>
+        {posts.map((post) => (
+          <CommunityCard
+            key={post.id}
+            title={post.title}
+            content={post.content}
+            author={post.author}
+            time={post.time}
+            commentCount={post.commentCount}
+            image={post.image}
+            category={post.category}
+            likeCount={post.likeCount}
+            onClick={() => console.log("카드 클릭:", post.id)}
+          />
+        ))}
       </div>
     </div>
   );
