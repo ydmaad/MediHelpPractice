766f06412cddeab716b8b9bb17aa13b7a85cd7d3
minae <ydmaad@gmail.com>
2025-02-20 21:23:03 +0900

    add: 캘린더 액션 및 리듀서 추가

--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
 import { drugReducer } from "./drugReducer";
+import { calendarReducer } from "./calendarReducer";
 
 // 리듀서를 하나로 합침
 export const rootReducer = combineReducers({
   auth: authReducer,
   drug: drugReducer,
+  calendar: calendarReducer,
 });
