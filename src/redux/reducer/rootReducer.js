cfedb5b6ed7fb70b529fd21706c39feb3d9787f4
minae-yang <minae.yang@mcbglobal.pro>
2025-03-10 17:14:10 +0900

    fix: rootReducer에 medicineReducer 추가

--- src/redux/reducer/rootReducer.js
+++ src/redux/reducer/rootReducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
 import { drugReducer } from "./drugReducer";
 import { calendarReducer } from "./calendarReducer";
+import { medicineReducer } from "./medicineReducer";
 
 // 리듀서를 하나로 합침
 export const rootReducer = combineReducers({
   auth: authReducer,
   drug: drugReducer,
   calendar: calendarReducer,
+  medicine: medicineReducer,
 });
