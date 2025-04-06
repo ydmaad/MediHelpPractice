7467bfe64ab0c662f04a6ffc104c795cfef96167
minae <ydmaad@gmail.com>
2025-04-06 23:17:50 +0900

    test

--- src/components/calendar/MedicineAddModal.jsx
+++ src/components/calendar/MedicineAddModal.jsx
@@ -142,6 +142,8 @@ const MedicineAddModal = ({ isOpen, onClose, onSubmit }) => {
 
   const amPmOption = ["오전", "오후"];
 
+  // 커밋 테스트
+
   return (
     <div
       className=" bg-gray-800 bg-opacity-50 flex items-center justify-center fixed inset-0 z-50"
