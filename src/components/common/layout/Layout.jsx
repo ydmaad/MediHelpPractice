c859128f0ca74c31ec8c1d4707763b38598e6110
minae <ydmaad@gmail.com>
2025-04-06 17:44:07 +0900

    fix: 배경이미지 메인 페이지일때만 보이도록 수정

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -2,19 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import background_img from "./../../../assets/background_img.svg";
+import { useLocation } from "react-router-dom";
 
 const Layout = ({ children }) => {
+  const location = useLocation();
+  const isMainPage = location.pathname === "/";
+
   return (
     <div className="flex flex-col min-h-screen bg-bg font-pretendard tracking-tight">
       <Header />
       <main
         className="flex-1"
-        style={{
-          backgroundImage: `url(${background_img})`,
-          backgroundSize: "cover",
-          backgroundPosition: "top center",
-          backgroundRepeat: "no-repeat",
-        }}
+        style={
+          isMainPage
+            ? {
+                backgroundImage: `url(${background_img})`,
+                backgroundSize: "cover",
+                backgroundPosition: "top center",
+                backgroundRepeat: "no-repeat",
+              }
+            : {}
+        }
       >
         <div className="w-[1000px] mx-auto mt-10">{children}</div>
       </main>
